17d910b4fbbd401c82a821c53f36d7d5ad121cb8
thuylinh227 <nguyenthithuylinh22072009@gmail.com>
2025-11-19 16:46:20 +0700

    Add files via upload

---
+++ insoduong.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class insoduong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao mot so: ");
+        int n= sc.nextInt();
+        int i=n;
+        while(i<0){
+            break;
+        }
+    }
+}
